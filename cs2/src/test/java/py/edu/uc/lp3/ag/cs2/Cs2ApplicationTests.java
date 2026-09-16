b2b1c108fababf3a0d27a09444b0fc67c10ef049
Ariel Gaona <agpy19@gmail.com>
2026-09-16 14:46:17 -0300

    Clase 16/09

---
+++ cs2/src/test/java/py/edu/uc/lp3/ag/cs2/Cs2ApplicationTests.java
@@ -0,0 +1,13 @@
+package py.edu.uc.lp3.ag.cs2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
